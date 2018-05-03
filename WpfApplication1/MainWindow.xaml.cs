81bcc324abc49865a18aa51e7fee0ec0905a54a2
m305-5\w401 <peng86david@gmail.com>
2018-05-03 17:05:17 +0800

    classwork w/ examples

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HeightSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            double heightVal = Math.Round(HeightSlider.Value,1);
+            HeightNum.Text = heightVal.ToString();
+            Canvas.SetLeft(Height, heightVal-50);
+        }
+    }
+}
